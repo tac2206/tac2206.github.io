8c75aee283e2d5d250fa3303e62f6b07bc122159
tac2206 <76880428+tac2206@users.noreply.github.com>
2021-01-08 10:44:35 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(h1).hover(function(){
+    $(h1).css("background-color", "yellow");
+    }, function(){
+    $(h1).css("background-color", "pink");
+  });
+
+
+
+}
